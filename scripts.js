3e0467953b82f2cd04298921faca2db9a5d41702
facingsouth <xin.d.liu@gmail.com>
2015-08-24 09:39:37 -0700

    javascript

--- scripts.js
+++ scripts.js
@@ -1,16 +1,36 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(arr){  
     // your code here
+    var largest = arr[0];
+    arr.forEach(function(ele) {
+      if (ele > largest) {
+        largest = ele;
+      };
+    });
+    return largest;
   },
   
-  reversed: function(){  
+  reversed: function(str){  
     // your code here
+    var new_str = "";
+    for(var i=str.length-1; i>-1; i--) {
+      new_str += str[i];
+    };
+    return new_str;
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(sentence){  
     // your code here
+    var striped_sentence = sentence.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var words = striped_sentence.split(" ");
+    var new_sentence = "";
+    words.forEach (function(ele){
+      var word = ele[0].toUpperCase;
+      new_sentence += ele.substring(1);
+      new_sentence += "_";
+    })
   },
 
   compareArrays: function(){ 
